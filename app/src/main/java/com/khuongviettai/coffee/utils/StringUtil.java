5575193f553c52a45b766d9d815b4707565f152f
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-04-22 19:44:38 +0700

    update

--- app/src/main/java/com/khuongviettai/coffee/utils/StringUtil.java
+++ app/src/main/java/com/khuongviettai/coffee/utils/StringUtil.java
@@ -8,4 +8,10 @@ public class StringUtil {
         return input == null || input.isEmpty() || ("").equals(input.trim());
     }
 
+    public static boolean isValidEmail(CharSequence target) {
+        if (target == null)
+            return false;
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
 }
