244b2742b31b1995204bd9c038cea3ecd1456fa0
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-05-13 06:01:06 +0700

    Revert "update"
    This reverts commit 5575193f553c52a45b766d9d815b4707565f152f.

--- app/src/main/java/com/khuongviettai/coffee/utils/StringUtil.java
+++ app/src/main/java/com/khuongviettai/coffee/utils/StringUtil.java
@@ -8,10 +8,4 @@ public class StringUtil {
         return input == null || input.isEmpty() || ("").equals(input.trim());
     }
 
-    public static boolean isValidEmail(CharSequence target) {
-        if (target == null)
-            return false;
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
-    }
-
 }
